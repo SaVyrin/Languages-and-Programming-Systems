e59893fe1a13f18795696b76fd7078b3cd5e113c
SaVyrin <sarajkinaa@gmail.com>
2022-03-21 20:48:06 +0300

    Edited and finished check_naming() Finished check_left_whitespaces_count() Finished _check_imports() Finished _check_file_ending_with_blank_line() Finished _check_maximum_line_length()

--- Task6/pep8_checker.py
+++ Task6/pep8_checker.py
@@ -45,23 +45,23 @@ class Pep8Checker:
         return lines_numeration
 
     def check_indentation(self):
-        self._text_lines = self._check_redundant_whitespaces()  # можно сразу исправить(не факт)
-        self._text_lines = self.check_def_whitespaces()  # можно сразу исправить(пробелы между аргументами и тд)
+        # self._text_lines = self._check_redundant_whitespaces()  # можно сразу исправить(не факт)
         self._text_lines = self._check_def_blank_lines()  # можно сразу исправить
-        self._check_maximum_line_length()
-        self._check_imports()
-        self._text_lines = self._check_file_ending_with_blank_line()  # можно сразу исправить
+        self.check_left_whitespaces_count()  # TODO:готово
+        self._text_lines = self._check_imports()  # TODO:готово
+        self._text_lines = self._check_file_ending_with_blank_line()  # TODO:готово
+        self._check_maximum_line_length()  # TODO:готово
 
     def check_naming(self):
         line_index = 1
         for line in self._text_lines:
-            if "class" in line:
+            if "class" in line.split():
                 self._check_class_naming(line, line_index)
 
-            elif "def" in line:
+            elif "def" in line.split():
                 self._check_def_naming(line, line_index)
 
-            elif "=" in line:
+            elif "=" in line.split():
                 self._check_variable_naming(line, line_index)
 
             line_index += 1
@@ -79,39 +79,36 @@ class Pep8Checker:
 
         return result_lines
 
-    def check_def_whitespaces(self):
-        text_lines = self._text_lines
+    def check_left_whitespaces_count(self):
+        line_index = 1
+        for line in self._text_lines:
+            left_spaces_count = 0
+            for sign in line:
+                if sign != " ":
+                    break
+                left_spaces_count += 1
 
-        result_lines = []
-        def_is_active = False
-        for line in text_lines:
+            if left_spaces_count % 4 != 0:
+                self._mistakes.append(f"Incorrect left spaces count in line: {line_index}")
 
-            if ":" in line:
-                def_is_active = True
-                result_lines.append(line)
-                continue
-
-            if def_is_active:
-                line = " ".join(line.split())
-                line = "    " + line
-
-            result_lines.append(line)
-        return result_lines
+            line_index += 1
 
     def _check_maximum_line_length(self):
-        text_lines = self._text_lines
-        return
+        line_index = 1
+        for line in self._text_lines:
+            if len(line) > 79:
+                self._mistakes.append(f"Too long line in line: {line_index}")
+            line_index += 1
 
     def _check_def_blank_lines(self):
         text_lines = self._text_lines
 
         result_lines = []
         for line in text_lines:
-
-            operands = line.split(" ")
-            if operands[0] == "\r":
+            if line.isspace() or line == "":
                 continue
 
+            operands = line.split()
             if operands[0] == "def" and line is not text_lines[0]:
                 result_lines.append("")
 
@@ -122,10 +119,36 @@ class Pep8Checker:
         result_lines = self._text_lines
 
         last_line_index = len(result_lines) - 1
-        last_line = result_lines[last_line_index]
+        if last_line_index >= 0:
+            last_line = result_lines[last_line_index]
 
-        if not last_line.isspace():
-            result_lines.append("")
+            if not last_line.isspace():
+                result_lines.append("")
+
+        return result_lines
+
+    def _check_imports(self):
+        result_lines = []
+
+        line_index = 1
+        last_import = ""
+        for line in self._text_lines:
+
+            line_array = line.split()
+            if "import" in line_array:
+                if "from" not in line_array:
+                    line_array = line.split()
+                    if len(line_array) > 2:
+                        self._mistakes.append(f"Incorrect importing in line: {line_index}")
+                last_import = line
+
+            elif last_import != "":
+                result_lines.append("")
+                result_lines.append("")
+                last_import = ""
+
+            line_index += 1
+            result_lines.append(line)
 
         return result_lines
 
@@ -135,7 +158,6 @@ class Pep8Checker:
         if splitted[0] == "def":
             function_name_and_args = splitted[1]
             function_name = function_name_and_args.split("(")[0].strip()
-
             if not function_name.islower():
                 self._mistakes.append(f"Incorrect function naming in line: {line_index}")
 
@@ -144,7 +166,6 @@ class Pep8Checker:
 
         if splitted[0] == "class":
             class_name = splitted[1]
-
             if "_" in class_name or not class_name[0].isupper():
                 self._mistakes.append(f"Incorrect class naming in line: {line_index}")
 
@@ -158,6 +179,3 @@ class Pep8Checker:
 
         if not variable_name.islower():
             self._mistakes.append(f"Incorrect variable naming in line: {line_index}")
-
-    def _check_imports(self):
-        return
